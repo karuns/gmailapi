c1ba6e7601be323e2fa9d45103dd4c7ed34d4edd
skarun <skarun@15.110.209.226>
2016-09-21 15:12:25 -0700

    cleaning up work

---
+++ src/test/java/TestQBMail.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import com.google.api.services.gmail.model.Message;
+import gclient.GmailAccount;
+import org.testng.annotations.Test;
+
+/* SEND and receive email thru google API
+ * DONE -- send and receive basic validation 
+ * Done Data provider
+ * Done Jenkins cloud
+ * methods with basic attachments
+ * More test cases
+ * PPTs
+ * 
+ */
+
+public class TestQBMail {	
+	@Test(dataProvider="data-provider-sample1",dataProviderClass=DataProviderClass.class)
+    public static void Test3(String from, String to, String subject, String body) throws InterruptedException, IOException {
+
+		GmailAccount user1 = new GmailAccount(from);
+		String id = user1.sendEmail(to, subject,body).getId();
+			
+		GmailAccount user2 = new GmailAccount("sohan.karun2@gmail.com");
+		Message m = user1.getEmail(id);
+		Thread.sleep(2000);
+		
+		System.out.println("PASS -"+m.getRaw());
+		System.out.println("PASS -"+m.toPrettyString().toString());
+	}
+	
+	@Test(enabled=false)
+	public static void Test_User1_User2() throws InterruptedException  {
+		GmailAccount user1 = new GmailAccount("sohan.karun1@gmail.com");
+		user1.sendEmail("sohan.karun2@gmail.com", "hyrsd","Hello from Sohan.karun1");
+			
+	}
+	
+	@Test(enabled=false)
+	public static void Test1() throws InterruptedException {
+		Thread.sleep(5000);
+		GmailAccount user2 = new GmailAccount("sohan.karun2@gmail.com");
+		user2.sendEmail("sohan.karun1@gmail.com", "eyrsd","Hello from Sohan.karun2");
+	}
+   
+}
